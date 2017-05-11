854bcd35225188c5d7950657503b0865e9ee4965
mkgask <mmp@zsw.jp>
2017-05-11 21:22:14 +0900

    [add] register up to 03

---
+++ storage/lp100k/03.py
@@ -0,0 +1,7 @@
+import re
+
+print([
+    len(re.sub('[^a-zA-Z]+', '', x))
+    for x
+    in "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.".split(' ')
+])
