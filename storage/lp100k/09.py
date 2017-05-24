f71ad25b0fe0207be5674ade5ff9c4e94d3ffe0d
mkgask <mmp@zsw.jp>
2017-05-24 22:33:11 +0900

    [add] Register up to 09

---
+++ storage/lp100k/09.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""09. Cipher Typoglycemia"""
+
+
+import random
+
+
+def typoglycemia(text):
+    return ' '.join([
+        word if len(word) <= 4 else word[0] + ''.join(random.sample(word[1:-1], len(word[1:-1]))) + word[-1]
+        for word
+        in text.split()
+    ])
+
+
+print(typoglycemia('I couldn\'t believe that I could actually understand what I was reading : the phenomenal power of the human mind .'))
