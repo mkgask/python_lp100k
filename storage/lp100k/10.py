5cf46a6fe6d5164fd53cc149bd93483a002b5cca
mkgask <mmp@zsw.jp>
2017-05-25 21:37:39 +0900

    [add] Register up to 10

---
+++ storage/lp100k/10.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""10. line count"""
+
+import os
+import sys
+import subprocess
+
+__dir__ = os.path.dirname(sys.argv[0])
+# __dir__ = os.path.dirname(os.path.abspath(__file__)) # maybe late
+
+
+def lineCount(filepath):
+    os.chdir(__dir__)
+    r = subprocess.run(["wc", filepath], stdout=subprocess.PIPE)
+    return r.stdout.split()[0]
+
+
+print(lineCount("./data/hightemp.txt"))
