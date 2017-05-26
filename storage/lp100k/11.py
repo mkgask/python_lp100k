76720f59745f5d50b1df2416514238ef247becb6
mkgask <mmp@zsw.jp>
2017-05-26 21:21:26 +0900

    [fix] Adjust output encoding

--- storage/lp100k/11.py
+++ storage/lp100k/11.py
@@ -12,7 +12,7 @@ __dir__ = os.path.dirname(sys.argv[0])
 def convertT2S(filepath):
     os.chdir(__dir__)
     r = subprocess.run(["expand", filepath], stdout=subprocess.PIPE)
-    return r.stdout.decode('utf-8')  # Adjust output
+    return r.stdout.encode('utf-8')  # Adjust output
 
 
 print(convertT2S("./data/hightemp.txt"))
